291f6d2026b5f630015bfc4302326e4edb1811e3
MaiRachlevsky <mrachlevskyreic@stuy.edu>
2018-04-12 00:15:31 -0400

    mazeSolver

---
+++ mazeSolver.java
@@ -0,0 +1,25 @@
+/** The mazeSolver class takes a maze and using recursion returns the boolean value of the statement
+    “the maze is navigable, having a legal travel path that is continuous from start point to existing treasure”. **/
+
+public class MazeSolver {
+
+    private Maze inProgress;
+
+    public MazeSolver(Maze maze) {
+	inProgress = new Maze(maze);
+    }
+
+    public boolean mazeSolution() {
+
+	if (inProgress.explorerIsOnA() == inProgress.TREASURE) {
+	    return true;
+	}
+	if (inProgress.explorerIsOnA() == inProgress.WALL) {
+	    return false;
+	}
+    }
+}
+
+
+
+    
