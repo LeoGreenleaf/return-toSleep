aaacee9bf6997d2a000983389e44f00a917e1fa6
LeoGreenleaf <34113324+LeoGreenleaf@users.noreply.github.com>
2018-04-13 01:58:27 -0400

    Update UserOfMazeSolver.java

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -3,7 +3,7 @@ public class UserOfMazeSolver{
 	public static void main(String[] commandLine) throws java.io.FileNotFoundException {
         	Maze testMaze = new Maze(commandLine[0], Integer.parseInt(commandLine[1]), Integer.parseInt(commandLine[2]));
 	       	MazeSolver testMazeSolver = new MazeSolver();
-    	}
-	System.out.println(testMaze);
-    	System.out.println(testMazeSolver.solve(testMaze));
+		System.out.println(testMaze);
+    		System.out.println(testMazeSolver.solve(testMaze));
+	}
 }
