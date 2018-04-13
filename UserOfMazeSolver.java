0c85d7ce0aa48ea722f40175c1555069750b9508
LeoGreenleaf <34113324+LeoGreenleaf@users.noreply.github.com>
2018-04-13 01:34:19 -0400

    Update UserOfMazeSolver.java

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,15 +1,11 @@
 /** Tests mazeSolver **/ 
 
 public class UserOfMazeSolver{
-  private static Displayer displayer;
-  public static void main(String[] commandLine) throws java.io.FileNotFoundException {
-        System.out.println();
-        Maze maze = new Maze(commandLine[0], Integer.parseInt(commandLine[1]), Integer.parseInt(commandLine[2]));
-        System.out.println(maze + System.lineSeparator());
-	       MazeSolver testMaze = new MazeSolver();
-        System.out.println(testSolver(testMaze, maze));
-    }
-    public static boolean testSolver( MazeSolver solver, Maze x) {
-	      return solver.mazeSolution(x);
-    }
+	public static void main(String[] commandLine) throws java.io.FileNotFoundException {
+        	Maze testMaze = new Maze(commandLine[0], Integer.parseInt(commandLine[1]), Integer.parseInt(commandLine[2]));
+	       	MazeSolver testMazeSolver = new MazeSolver();
+    	}
+	System.out.println(testMaze);
+    	System.out.println(testMazeSolver.solve(testMaze));
+    	}
 }
